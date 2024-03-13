e3b9275747b3e7c7710a459391e8368f21b448dc
Michael Hoisie <hoisie@google.com>
2024-03-13 18:40:11 +0000

    Add empty application for Coverage Test

---
+++ app/src/test/java/com/example/coveragetest/ExampleUnitTest.java
@@ -0,0 +1,22 @@
+package com.example.coveragetest;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.Robolectric;
+import org.robolectric.RobolectricTestRunner;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+@RunWith(RobolectricTestRunner.class)
+public class ExampleUnitTest {
+
+  @Test
+  public void addition_isCorrect() {
+    Robolectric.setupActivity(MainActivity.class);
+  }
+}
